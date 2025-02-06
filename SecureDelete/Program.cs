cbf89e9581186c86afe1aac5c6d9bb414b296d0f
Bruce Van Horn <bvanhorn@cleartechnologies.net>
2025-02-06 15:52:57 -0600

    cleaning things up

--- SecureDelete/Program.cs
+++ SecureDelete/Program.cs
@@ -4,9 +4,9 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 
-class SecureDelete
+internal static class SecureDelete
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         if (args.Length != 1)
         {
@@ -40,26 +40,22 @@ class SecureDelete
         var fileSize = fileInfo.Length;
 
         // Generate a random AES key (not stored, so recovery is impossible)
-        using (Aes aes = Aes.Create())
+        using var aes = Aes.Create();
+        aes.KeySize = 256;
+        aes.GenerateKey();
+        aes.GenerateIV();
+
+        using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Write, FileShare.None);
+        using var cs = new CryptoStream(fs, aes.CreateEncryptor(), CryptoStreamMode.Write);
+        var buffer = new byte[4096];
+        RandomNumberGenerator.Fill(buffer); 
+
+        long written = 0;
+        while (written < fileSize)
         {
-            aes.KeySize = 256;
-            aes.GenerateKey();
-            aes.GenerateIV();
-
-            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write, FileShare.None))
-            using (CryptoStream cs = new CryptoStream(fs, aes.CreateEncryptor(), CryptoStreamMode.Write))
-            {
-                var buffer = new byte[4096]; // 4KB buffer
-                RandomNumberGenerator.Fill(buffer); // Fill with random bytes
-
-                long written = 0;
-                while (written < fileSize)
-                {
-                    var toWrite = (int)Math.Min(buffer.Length, fileSize - written);
-                    cs.Write(buffer, 0, toWrite);
-                    written += toWrite;
-                }
-            }
+            var toWrite = (int)Math.Min(buffer.Length, fileSize - written);
+            cs.Write(buffer, 0, toWrite);
+            written += toWrite;
         }
     }
 }
